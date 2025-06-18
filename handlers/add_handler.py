ac21ccf74b0b506974d332eda4aeb9e1ef37e21a
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-18 15:02:38 +0300

    Remind, cancel, event priority handlers

--- handlers/add_handler.py
+++ handlers/add_handler.py
@@ -1,24 +1,27 @@
 import logging
+from datetime import datetime, timedelta
 
 from aiogram import types
 from aiogram import Router
 from aiogram.enums import ParseMode
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from sqlalchemy.orm import sessionmaker
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 
+from const.event_status import EventStatus
+from const.priority_status import PriorityStatus
 from keyboards import for_event_type
 from aiogram import F
 
 from bot_state.add_event_state import AddEventState
 from config import BotConfig
 from db import Event, engine, User
+from keyboards.for_event_type import cancel_button
 
 config = BotConfig()
 router = Router()
 Session = sessionmaker(bind=engine)
-session = Session()
 new_event = Event()
 
 
@@ -28,7 +31,7 @@ async def cmd_add(message: Message, state: FSMContext):
         new_event.telegram_chat_id = message.chat.id
         await state.set_state(AddEventState.adding_title)
         await message.answer(
-            text="Введите название события", parse_mode=ParseMode.HTML
+            text="Введите название события", parse_mode=ParseMode.HTML, reply_markup=cancel_button()
         )
     except Exception as e:
         logging.error(e)
@@ -38,9 +41,9 @@ async def cmd_add(message: Message, state: FSMContext):
 async def event_title(message: Message, state: FSMContext):
     try:
         await state.set_state(AddEventState.adding_description)
-        new_event.title = message.text  # Сохраняем название события
+        new_event.title = message.text
         await message.answer(
-            text="Введите описание события"
+            text="Введите описание события", reply_markup=cancel_button()
         )
     except Exception as e:
         logging.error(e)
@@ -50,7 +53,7 @@ async def event_title(message: Message, state: FSMContext):
 async def event_description(message: Message, state: FSMContext):
     try:
         await state.set_state(AddEventState.adding_status)
-        new_event.description = message.text  # Сохраняем описание события
+        new_event.description = message.text
         await message.answer(
             text="Выберите статус события", reply_markup=for_event_type.status_inline_kb()
         )
@@ -62,29 +65,57 @@ async def event_description(message: Message, state: FSMContext):
 async def event_status(callback: types.CallbackQuery, state: FSMContext):
     try:
         await state.set_state(AddEventState.adding_repeatable)
-        new_event.status = callback.data.__str__()
-        await callback.message.answer(text="Выберите когда повторять событие",
+        new_event.status = EventStatus(callback.data)
+        await callback.message.answer(text="Выберите когда напомнить про событие",
                                       reply_markup=for_event_type.repeatable_inline_kb())
     except Exception as e:
         logging.error(e)
 
+
 @router.callback_query(F.data, AddEventState.adding_repeatable)
 async def event_repeatable(callback: types.CallbackQuery, state: FSMContext):
     try:
-        await state.set_state(AddEventState.adding_priority)
+        await state.set_state(AddEventState.adding_remind_at)
         new_event.repeat_type = callback.data.__str__()
-        await callback.message.answer(text="Выберите приоритет события",
-                                      reply_markup=for_event_type.priority_inline_kb())
+        await callback.message.answer(text="Напишите время напоминания в формате 'HH:MM'")
     except Exception as e:
         logging.error(e)
 
-@router.callback_query(F.data, AddEventState.adding_priority)
-async def event_priority(callback: types.CallbackQuery, state: FSMContext):
+
+@router.message(F.text, AddEventState.adding_remind_at)
+async def event_remind_at(message: Message, state: FSMContext):
     try:
-        new_event.priority = callback.data.__str__()  # Сохраняем приоритет события
-        new_event.user_id = session.query(User).filter_by(telegram_user_id=callback.from_user.id).first().id
-        session.add(new_event)
-        session.commit()
-        await state.set_data({})
+        await state.set_state(AddEventState.adding_priority)
+        time_input = message.text.strip()
+        time = datetime.strptime(time_input, "%H:%M").time()
+        now = datetime.now()
+        remind_time = datetime.combine(now.date(), time)
+        if remind_time < now:
+            remind_time += timedelta(days=1)
+
+        new_event.remind_at = remind_time
+        await message.answer(text="Выберите приоритет события",
+                             reply_markup=for_event_type.priority_inline_kb())
     except Exception as e:
         logging.error(e)
+
+
+@router.callback_query(F.data, AddEventState.adding_priority)
+async def event_priority(callback: types.CallbackQuery, state: FSMContext):
+    with Session() as session:
+        try:
+            new_event.priority = PriorityStatus(callback.data)
+            new_event.user_id = session.query(User).filter_by(telegram_user_id=callback.from_user.id).first().id
+            session.add(new_event)
+            session.commit()
+            await state.set_data({})
+        except Exception as e:
+            logging.error(e)
+
+
+@router.callback_query(F.data == 'cancel')
+async def cancel_event(message: Message, state: FSMContext):
+    with Session() as session:
+        session.flush()
+        await state.clear()
+        await message.answer(text="Действие добавления отменено!")
