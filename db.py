0fc2d23fb5f2ba4092e26125a14898914ef395f3
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-18 15:03:26 +0300

    Relationship between events and users tables

--- db.py
+++ db.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 
 from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey, Boolean, DATETIME, Nullable, BigInteger
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from dotenv import load_dotenv
 from sqlalchemy.dialects.postgresql import ENUM as PgEnum
-from sqlalchemy.dialects.postgresql import TIMESTAMP as PgTime
-from sqlalchemy.dialects.postgresql import DATE as PgDate
+from sqlalchemy import DateTime
 import os
 
 from const.event_status import EventStatus
@@ -25,20 +24,26 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     telegram_user_id = Column(Integer, unique=True, nullable=False)
     username = Column(String(30), nullable=False)
+    events = relationship("Event", back_populates="user")
 
 class Event(Base):
-    __tablename__ = 'event'
+    __tablename__ = 'events'
 
     id = Column(Integer, primary_key=True)
     title = Column(String(20), nullable=False)
     description = Column(Text, nullable=True)
-    status = Column(PgEnum(EventStatus, name='event_status_enum', create_type=False), nullable=False, default=EventStatus.TO_DO)
-    priority = Column(PgEnum(PriorityStatus, name='priority_status_enum', create_type=False), nullable=False, default=PriorityStatus.MEDIUM)
-    created_at = Column(PgDate, nullable=False, default=datetime.now())
-    updated_at = Column(PgDate, nullable=False, default=datetime.now)
+    status = Column(PgEnum(EventStatus, name='event_status_enum', create_type=False), nullable=False,
+                    default=EventStatus.TO_DO)
+    priority = Column(PgEnum(PriorityStatus, name='priority_status_enum', create_type=False), nullable=False,
+                      default=PriorityStatus.MEDIUM)
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    updated_at = Column(DateTime, nullable=False, default=datetime.now)
     repeatable = Column(Boolean, nullable=False, default=False)
-    remind_at = Column(PgTime, nullable=False, default=datetime.now())
-    repeat_type = Column(PgEnum(RepeatType, name='repeat_type_enum', create_type=False), nullable=False, default=RepeatType.EVERY_DAY)
+    sent = Column(Boolean, nullable=False, default=False)
+    remind_at = Column(DateTime, nullable=False, default=datetime.now)
+    repeat_type = Column(PgEnum(RepeatType, name='repeat_type_enum', create_type=False), nullable=False,
+                         default=RepeatType.ONLY_DAY)
     chat_name = Column(String(10), nullable=True, default=None)
     telegram_chat_id = Column(BigInteger, nullable=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user = relationship("User", back_populates="events")
