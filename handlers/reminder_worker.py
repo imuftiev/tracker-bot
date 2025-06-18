978b2360f2303ea0bae94c32774c99d789b0a28b
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-18 15:01:36 +0300

    Worker added
    -function constantly checking if the events wasn't sent to chat and sending it

---
+++ handlers/reminder_worker.py
@@ -0,0 +1,37 @@
+import asyncio
+from datetime import datetime, timezone
+from sqlalchemy.orm import sessionmaker
+from db import engine, Event
+from aiogram import Bot
+
+Session = sessionmaker(bind=engine)
+
+async def reminder_worker(bot : Bot):
+    while True:
+        try:
+            now = datetime.now(timezone.utc)
+            with Session() as session:
+                events = session.query(Event).filter(
+                    Event.remind_at <= now,
+                    Event.sent == False
+                ).all()
+
+                for event in events:
+                    await bot.send_message(
+                        chat_id=event.telegram_chat_id,
+                        text=(f"🔔<strong> Событие: </strong> {event.title}\n"
+                              f"📝<strong> Описание: </strong> {event.description}\n"
+                              f"❓<strong> Статус: </strong> {event.status.value}\n"
+                              f"✔️<strong> Приоритет: </strong> {event.priority.value}\n"),
+                        parse_mode="HTML"
+                    )
+
+                    event.sent = True
+                    session.add(event)
+
+                session.commit()
+
+        except Exception as e:
+            print(f"[ReminderWorker] Ошибка: {e}")
+
+        await asyncio.sleep(30)
