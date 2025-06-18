67536cc15f6162ec6f52a8818958e10acb15ec56
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-18 15:02:58 +0300

    Worker task

--- bot.py
+++ bot.py
@@ -9,18 +9,21 @@ from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 
 from config import BotConfig
-from handlers import start_handler, help_handler, add_handler
+from db import Event
+from handlers import start_handler, help_handler, add_handler, reminder_worker
+from handlers import reminder_worker as rm
 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 dp = Dispatcher()
 botconfig = BotConfig()
-
+event = Event()
 
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp.include_routers(start_handler.router, help_handler.router, add_handler.router)
+    asyncio.create_task(rm.reminder_worker(bot))
     await dp.start_polling(bot)
 
 
