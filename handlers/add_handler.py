5776f8a1f3bcd743ad116ad9d18e26fff052f26e
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-19 13:43:21 +0300

    Stack storage for bot state,back and cancel inline buttons added

--- handlers/add_handler.py
+++ handlers/add_handler.py
@@ -8,51 +8,79 @@ from aiogram.types import Message, CallbackQuery
 from sqlalchemy.orm import sessionmaker
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
+from handlers.time_function import set_time_remind
 
 from const.event_status import EventStatus
 from const.priority_status import PriorityStatus
+from const.repeatable_type import RepeatType, RepeatDays
 from keyboards import for_event_type
 from aiogram import F
 
-from bot_state.add_event_state import AddEventState
+from bot_state.add_event_state import AddEventState, RepeatableEventState
 from config import BotConfig
 from db import Event, engine, User
-from keyboards.for_event_type import cancel_button
+from keyboards.for_event_type import cancel_button, cancel_back_button
 
 config = BotConfig()
 router = Router()
 Session = sessionmaker(bind=engine)
 new_event = Event()
 
+"""
+    Для реализации возможности диалога с ботом и сохранением его состояния был использован 
+    принцип Конечного Автомата с применением FSMContext
+    Инициализация начального состояния происходит путем ввода команды – /add
+    Сброс состояния бота – /cancel
+"""
+
+
+async def push_state(state: FSMContext, new_state):
+    data = await state.get_data()
+    history = data.get("history", [])
+    current_state = await state.get_state()
+    if current_state:
+        history.append(current_state)
+    await state.update_data(history=history)
+    await state.set_state(new_state)
+
+
+""" Обработчик команды /add """
+
 
 @router.message(StateFilter(None), Command("add"))
 async def cmd_add(message: Message, state: FSMContext):
     try:
         new_event.telegram_chat_id = message.chat.id
-        await state.set_state(AddEventState.adding_title)
+        await push_state(state, AddEventState.adding_title)
         await message.answer(
-            text="Введите название события", parse_mode=ParseMode.HTML, reply_markup=cancel_button()
+            text="Введите название события", parse_mode=ParseMode.HTML, reply_markup=for_event_type.cancel_button()
         )
     except Exception as e:
         logging.error(e)
+
+
+""" Обработчик сообщения – Название ивента"""
 
 
 @router.message(AddEventState.adding_title, F.text)
 async def event_title(message: Message, state: FSMContext):
     try:
-        await state.set_state(AddEventState.adding_description)
+        await push_state(state, AddEventState.adding_description)
         new_event.title = message.text
         await message.answer(
-            text="Введите описание события", reply_markup=cancel_button()
+            text="Введите описание события", reply_markup=cancel_back_button()
         )
     except Exception as e:
         logging.error(e)
 
 
+""" Обработчик сообщения – Описание ивента"""
+
+
 @router.message(F.text, AddEventState.adding_description)
 async def event_description(message: Message, state: FSMContext):
     try:
-        await state.set_state(AddEventState.adding_status)
+        await push_state(state, AddEventState.adding_status)
         new_event.description = message.text
         await message.answer(
             text="Выберите статус события", reply_markup=for_event_type.status_inline_kb()
@@ -61,40 +89,67 @@ async def event_description(message: Message, state: FSMContext):
         logging.error(e)
 
 
+""" Обработчик Callback вызова – Статус ивента"""
+
+
 @router.callback_query(F.data, AddEventState.adding_status)
 async def event_status(callback: types.CallbackQuery, state: FSMContext):
     try:
-        await state.set_state(AddEventState.adding_repeatable)
+        match callback.data:
+            case "cancel":
+                await cancel_button(callback, state)
+                return
+            case "back":
+                await back(callback, state)
+                return
+        await push_state(state, AddEventState.adding_repeatable)
         new_event.status = EventStatus(callback.data)
-        await callback.message.answer(text="Выберите когда напомнить про событие",
-                                      reply_markup=for_event_type.repeatable_inline_kb())
+        await callback.message.edit_reply_markup(reply_markup=None)
+        await callback.message.answer(
+            text="Когда напомнить про событие",
+            reply_markup=for_event_type.repeatable_inline_kb()
+        )
     except Exception as e:
-        logging.error(e)
+        logging.error(f"Ошибка в event_status: {e}")
+
+
+""" Обработчик Callback вызова – Когда напоминать про событие"""
 
 
 @router.callback_query(F.data, AddEventState.adding_repeatable)
 async def event_repeatable(callback: types.CallbackQuery, state: FSMContext):
     try:
-        await state.set_state(AddEventState.adding_remind_at)
-        new_event.repeat_type = callback.data.__str__()
-        await callback.message.answer(text="Напишите время напоминания в формате 'HH:MM'")
+        match callback.data:
+            case RepeatType.EVERY_WEEK.value:
+                await push_state(state, RepeatableEventState.adding_day)
+                new_event.repeatable = True
+                await callback.message.edit_reply_markup(reply_markup=None)
+                await callback.message.answer(text="Напоминать в определенный\nдень недели, или каждый день",
+                                              reply_markup=for_event_type.choose_day_kb())
+            case _:
+                await push_state(state, AddEventState.adding_remind_at)
+                new_event.repeat_type = RepeatType(callback.data)
+                await callback.message.edit_reply_markup(reply_markup=None)
+                await callback.message.answer(text="Напишите время напоминания в формате '12:30' или '1230'")
     except Exception as e:
         logging.error(e)
+
+
+""" Обработчик сообщения – Время напоминания 'HH:MM' """
 
 
 @router.message(F.text, AddEventState.adding_remind_at)
 async def event_remind_at(message: Message, state: FSMContext):
     try:
-        await state.set_state(AddEventState.adding_priority)
-        time_input = message.text.strip()
-        time = datetime.strptime(time_input, "%H:%M").time()
         now = datetime.now()
-        remind_time = datetime.combine(now.date(), time)
+        remind_time = await set_time_remind(message, state)
+        if remind_time is None:
+            return
         if remind_time < now:
             remind_time += timedelta(days=1)
-
         new_event.remind_at = remind_time
-        await message.answer(text="Выберите приоритет события",
+        await push_state(state, AddEventState.adding_priority)
+        await message.answer(text="Приоритет события",
                              reply_markup=for_event_type.priority_inline_kb())
     except Exception as e:
         logging.error(e)
@@ -104,18 +159,62 @@ async def event_remind_at(message: Message, state: FSMContext):
 async def event_priority(callback: types.CallbackQuery, state: FSMContext):
     with Session() as session:
         try:
+            await callback.message.edit_reply_markup(reply_markup=None)
             new_event.priority = PriorityStatus(callback.data)
             new_event.user_id = session.query(User).filter_by(telegram_user_id=callback.from_user.id).first().id
             session.add(new_event)
             session.commit()
-            await state.set_data({})
+            await state.clear()
+            await callback.message.answer(text=config.success_text)
         except Exception as e:
             logging.error(e)
 
 
 @router.callback_query(F.data == 'cancel')
-async def cancel_event(message: Message, state: FSMContext):
+async def cancel_button(callback: CallbackQuery, state: FSMContext):
     with Session() as session:
         session.flush()
+        await callback.message.edit_reply_markup(reply_markup=None)
         await state.clear()
-        await message.answer(text="Действие добавления отменено!")
+        await callback.message.answer(text=config.cancel_text)
+
+
+@router.callback_query(F.data, RepeatableEventState.adding_day)
+async def every_day_event(state: FSMContext):
+    try:
+        await state.set_state(AddEventState.adding_remind_at)
+    except Exception as e:
+        logging.error(e)
+
+
+@router.message(F.text)
+async def text_message(message: Message):
+    await message.answer(text=config.default_text)
+
+
+@router.callback_query(F.data == 'back')
+async def back(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    history = data.get("history", [])
+
+    previous_state = history.pop()
+    await state.update_data(history=history)
+    await state.set_state(previous_state)
+
+    if previous_state == AddEventState.adding_title:
+        await callback.message.edit_text("Введите название события")
+    elif previous_state == AddEventState.adding_description:
+        await callback.message.edit_text("Введите описание события")
+    elif previous_state == AddEventState.adding_status:
+        await callback.message.edit_text(
+            "Выберите статус события", reply_markup=for_event_type.status_inline_kb()
+        )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data == RepeatDays.ALL_DAYS.value)
+async def all_days_event(callback: CallbackQuery, state: FSMContext):
+    new_event.repeat_type = RepeatType(callback)
+    await callback.message.answer(text="Напишите время напоминания в формате '12:30' или '1230'")
+    await push_state(state, AddEventState.adding_remind_at)
