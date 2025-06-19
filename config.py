2529d377c7ba9b19a68e13416559844dd1720dbe
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-19 13:44:18 +0300

    import text from .json file

--- config.py
+++ config.py
@@ -10,3 +10,8 @@ class BotConfig:
         self.start_text = data['greetings']['user-unknown']
         self.start_text_user = data['greetings']['user-registered']
         self.help_text = data['help']
+        self.cancel_title = data['cancel']['title']
+        self.cancel_text = data['cancel']['message']
+        self.default_text = data['default']
+        self.back_text = data['back']
+        self.success_text = data['success']
