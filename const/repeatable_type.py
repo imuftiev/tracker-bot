59b2b912a335ff8e4951acd2b2893a0ac174f9d3
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-19 13:46:57 +0300

    repeatdays class added and translate for russian

--- const/repeatable_type.py
+++ const/repeatable_type.py
@@ -2,7 +2,13 @@ from enum import Enum
 
 
 class RepeatType(Enum):
-    EVERY_DAY = 'EVERY_DAY'
-    EVERY_WEEK = 'EVERY_WEEK'
-    EVERY_MONTH = 'EVERY_MONTH'
-    EVERY_YEAR = 'EVERY_YEAR'
+    EVERY_DAY = 'Ежедневно'
+    EVERY_WEEK = 'Еженедельно'
+    EVERY_MONTH = 'Ежемесячно'
+    EVERY_YEAR = 'Ежегодно'
+    ONLY_DAY = 'Только сегодня'
+    IN_PARTICULAR_DAY = 'Определенный день'
+
+class RepeatDays(Enum):
+    ALL_DAYS = 'Все дни'
+    EXACT_DAY = 'Определенный день'
