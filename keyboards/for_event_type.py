f1df073c7758f6ed9d914979f9d8c292d6337d74
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-19 13:45:39 +0300

    Inline buttons change

--- keyboards/for_event_type.py
+++ keyboards/for_event_type.py
@@ -1,11 +1,13 @@
-from tkinter import EventType
-
 from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram import types
 
+import config
 from const.event_status import EventStatus
 from const.priority_status import PriorityStatus
+from const.repeatable_type import RepeatType, RepeatDays
+
+config = config.BotConfig()
 
 
 def get_kb() -> ReplyKeyboardMarkup:
@@ -18,20 +20,39 @@ def get_kb() -> ReplyKeyboardMarkup:
 
 def cancel_button() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(text="Cancel", callback_data="cancel"))
+    builder.row(types.InlineKeyboardButton(text=config.cancel_title, callback_data="cancel"))
+    return builder.as_markup(resize_keyboard=True)
+
+
+def cancel_back_button() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(text=config.back_text, callback_data="back"))
+    builder.row(types.InlineKeyboardButton(text=config.cancel_title, callback_data="cancel"))
+    return builder.as_markup(resize_keyboard=True)
+
+
+def choose_day_kb() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    inline_keyboard = [
+        types.InlineKeyboardButton(text="Все дни недели", callback_data=RepeatDays.ALL_DAYS.value),
+        types.InlineKeyboardButton(text="В конкретные дни", callback_data=RepeatDays.EXACT_DAY.value),
+    ]
+    for InlineKeyboardButton in inline_keyboard:
+        builder.row(InlineKeyboardButton, width=8)
     return builder.as_markup(resize_keyboard=True)
 
 
 def repeatable_inline_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     inline_keyboard = [
-        types.InlineKeyboardButton(text="Каждый день", callback_data="EVERY_DAY"),
-        types.InlineKeyboardButton(text="Каждую неделю", callback_data="EVERY_WEEK"),
-        types.InlineKeyboardButton(text="Каждый месяц", callback_data="EVERY_MONTH"),
-        types.InlineKeyboardButton(text="Каждый год", callback_data="EVERY_YEAR"),
-        types.InlineKeyboardButton(text="Только сегодня", callback_data="ONLY_TODAY"),
-        types.InlineKeyboardButton(text="В определенный день", callback_data="O"),
-        types.InlineKeyboardButton(text="Отмена", callback_data="cancel")]
+        types.InlineKeyboardButton(text="Каждый день", callback_data=RepeatType.EVERY_DAY.value),
+        types.InlineKeyboardButton(text="Каждую неделю", callback_data=RepeatType.EVERY_WEEK.value),
+        types.InlineKeyboardButton(text="Каждый месяц", callback_data=RepeatType.EVERY_MONTH.value),
+        types.InlineKeyboardButton(text="Каждый год", callback_data=RepeatType.EVERY_YEAR.value),
+        types.InlineKeyboardButton(text="Только сегодня", callback_data=RepeatType.ONLY_DAY.value),
+        types.InlineKeyboardButton(text="Определенный день", callback_data=RepeatType.IN_PARTICULAR_DAY.value),
+        types.InlineKeyboardButton(text=config.back_text, callback_data="back"),
+        types.InlineKeyboardButton(text=config.cancel_title, callback_data="cancel")]
     for InlineKeyboardButton in inline_keyboard:
         builder.row(InlineKeyboardButton)
     return builder.as_markup(resize_keyboard=True)
@@ -44,10 +65,10 @@ def priority_inline_kb() -> InlineKeyboardMarkup:
         types.InlineKeyboardButton(text="Средний", callback_data=PriorityStatus.HIGH.value),
         types.InlineKeyboardButton(text="Высокий", callback_data=PriorityStatus.MEDIUM.value),
         types.InlineKeyboardButton(text="Критический", callback_data=PriorityStatus.LOW.value),
-        types.InlineKeyboardButton(text="Отмена", callback_data="cancel")
     ]
     for InlineKeyboardButton in inline_keyboard:
         builder.row(InlineKeyboardButton)
+    builder.row(types.InlineKeyboardButton(text=config.cancel_title, callback_data="cancel"))
     return builder.as_markup(resize_keyboard=True)
 
 
@@ -56,5 +77,6 @@ def status_inline_kb() -> InlineKeyboardMarkup:
     builder.add(types.InlineKeyboardButton(text="Выполнить", callback_data=EventStatus.TO_DO.value))
     builder.add(types.InlineKeyboardButton(text="В процессе", callback_data=EventStatus.PROCESSING.value))
     builder.add(types.InlineKeyboardButton(text="Выполнено", callback_data=EventStatus.DONE.value))
-    builder.add(types.InlineKeyboardButton(text="Отмена", callback_data='cancel'))
+    builder.row(types.InlineKeyboardButton(text=config.back_text, callback_data="back"),
+                types.InlineKeyboardButton(text=config.cancel_title, callback_data="cancel"),width=8)
     return builder.as_markup(resize_keyboard=True)
