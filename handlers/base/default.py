6a982f27a03edf6e967a59b23fa997206500b241
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-06-20 11:14:24 +0300

    Default message handler

---
+++ handlers/base/default.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram import F
+from aiogram.types import Message
+
+from config import BotConfig
+
+router = Router()
+config = BotConfig()
+
+@router.message(F.text)
+async def default_message_handler(message: Message):
+    await message.answer(text=config.default_text)
